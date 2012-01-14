49425d19d7d031f50c56cfc3f4f9736fa19e1e3d
Joel Birchler <joel@joelbirchler.com>
2012-01-13 22:18:37 -0800

    added rocks

--- host.js
+++ host.js
@@ -1,6 +1,7 @@
 $(function() {
   
-  var botServer = "http://localhost:4567";
+  var botServer = "http://localhost:4567",
+    numberOfRocks = 60;
   
   var gridWidth = 100,
     gridHeight = 50,
@@ -9,17 +10,36 @@ $(function() {
     tileWidth = canvasWidth/gridWidth,
     tileHeight = canvasHeight/gridHeight,
     canvasCtx = $('#canvas').get(0).getContext('2d');
+    
+  var rocks = [];
+  _.times(numberOfRocks, function(){
+    rocks.push(randomCoordinate())
+  });
+  
+  function randomCoordinate() {
+    return {x: Math.floor(Math.random() * gridWidth), y: Math.floor(Math.random() * gridHeight)}
+  }
   
   function canMoveTo(x, y) {
     if (x < 0 || y < 0 || x > gridWidth - 1 || y > gridHeight - 1 ) { return false; }
+    if (_.any(rocks, function(rock) {return (x === rock.x && y === rock.y);})) { return false; }
+    
     return true;
   }
   
   function draw() {
     canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
     
-    var plotX = bot.x * tileWidth, plotY = bot.y * tileHeight;
-    canvasCtx.fillStyle = "rgb("+ Math.floor(150 + bot.energy) +",0,0)";  
+    _.each(rocks, function(rock) {
+      plotTile(rock.x, rock.y, 100, 100, 100);
+    });
+    
+    plotTile(bot.x, bot.y, Math.floor(150 + bot.energy), 0, 0);
+  }
+  
+  function plotTile(x, y, r, g, b) {
+    var plotX = x * tileWidth, plotY = y * tileHeight;
+    canvasCtx.fillStyle = "rgb("+r+","+g+","+b+")";  
     canvasCtx.fillRect (plotX, plotY, tileWidth, tileHeight);
   }
   
